8b8e3d0b02bf8669f2b20f0c73df8358150c6238
ESHWARGUNDLA <84794875+ESHWARGUNDLA@users.noreply.github.com>
2023-08-14 17:59:10 +0000

    intro

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-console.log("Hello")
+// console.log("Hello")
+// const state={
+//     taskList:[
+//         {
+//             image:"",
+//             title:"",
+//             type:"",
+//             description:"",
+
+//         },
+//         {
+//             image:"",
+//             title:"",
+//             type:"",
+//             description:"",
+
+//         },
+//         {
+//             image:"",
+//             title:"",
+//             type:"",
+//             description:"",
+
+//         },
+//         {
+//             image:"",
+//             title:"",
+//             type:"",
+//             description:"",
+
+//         },
+//         {
+//             image:"",
+//             title:"",
+//             type:"",
+//             description:"",
+
+//         },
+//     ]
+// }
+
+
+const state ={
+    taskList:[]
+};
+
+// DOM
+const taskContents = document.querySelector(".task__contents");
+const taskModal = document.querySelector(".task__modal__body");
+
+// console.log(taskContents);
+// console.log(taskModal);
+
+const htmltaskContents=({id,title,description,type,url}) => {
+
+}
